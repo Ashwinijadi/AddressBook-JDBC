d0ac23330793d9bd359f97b1ae6b120813c58897
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-02 07:33:29 -0800

    added: JDBC used to connect and update addressbook in Database are added

--- Test/com/capgemini/addressbook/Address_BookService_Test.java
+++ Test/com/capgemini/addressbook/Address_BookService_Test.java
@@ -5,17 +5,24 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import com.capgemini.addressbook.Address_Book_Service.IOService;
-
 class Address_BookService_Test {
 
 	@Test
-	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws SQLException {
+	public void givenAddressBookInDB_WhenRetrieved_ShouldMatchAddressBookCount() throws SQLException {
 		Address_Book_Service addressBookService = new Address_Book_Service();
-		List<Address_Book_Data> addressBookData = addressBookService.readData(IOService.DB_IO);
+		List<Address_Book_Data> addressBookData = addressBookService.readData();
 		System.out.println(addressBookData);
 		Assert.assertEquals(4, addressBookData.size());
+	}
 
+	@Test
+	public void givenNewCityForPerson_WhenUpdatedUsingPreparedStatement_ShouldSyncWithDB() throws SQLException {
+		Address_Book_Service addressBookService = new Address_Book_Service();
+		List<Address_Book_Data> addressBookData = addressBookService.readData();
+		addressBookService.update("Sneha","hyd");
+		boolean result = addressBookService.checkContactInSyncWithDB("Sneha");
+		Assert.assertTrue(result);
+		System.out.println(addressBookData);
 	}
 
 }
